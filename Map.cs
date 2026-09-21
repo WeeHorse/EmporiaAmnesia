c87e1e0c47ace59d561f2e946d3dbdd7e0ed2a4e
WeeHorse <benjaminberglund@gmail.com>
2026-09-21 14:17:00 +0200

    Map.locations and GetLocation done

--- Map.cs
+++ Map.cs
@@ -1,4 +1,20 @@
 class Map
 {
+  private Location?[][] locations =
+  {
+    new Location?[5] {null,               null,               new Escalator1(),         new Roof(),           null},
+    new Location?[5] {new Foyer(),        new Escalator2(),   new CorridorA(),          null,                 null},
+    new Location?[5] {new ToiletStall(),  null,               new OutsideDryCleaner(),  new CorridorB(),      null},
+    new Location?[5] {null,               null,               new DryCleaner(),         new SecurityOffice(), new TaxiStation()}
+  };
+
+
+
+  public Location GetLocation(int row, int col)
+  {
+    return locations[row][col]!; // ! assertion, we promise this will exist
+  }
+
+
 
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
+
+Map map = new Map();
+
+Location loc = map.GetLocation(2, 0);
+
+Console.Write(loc);
+
